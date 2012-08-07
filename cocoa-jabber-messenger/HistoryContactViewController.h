06be193a916a43c3fae23973f4940755809d94bc
陈小实 <chenshuoshi@gmail.com>
2012-08-07 20:19:32 +0800

    ui改版

---
+++ cocoa-jabber-messenger/HistoryContactViewController.h
@@ -0,0 +1,13 @@
+//
+//  HistoryContactViewController.h
+//  SinaUC
+//
+//  Created by 硕实 陈 on 12-8-6.
+//  Copyright (c) 2012年 NHN Corporation. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface HistoryContactViewController : NSViewController
+
+@end
---
+++ cocoa-jabber-messenger/HistoryContactViewController.m
@@ -0,0 +1,23 @@
+//
+//  HistoryContactViewController.m
+//  SinaUC
+//
+//  Created by 硕实 陈 on 12-8-6.
+//  Copyright (c) 2012年 NHN Corporation. All rights reserved.
+//
+
+#import "HistoryContactViewController.h"
+
+@implementation HistoryContactViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
